ad162694756a2ecd91611cf7faededd996085f09
Vagacoder <30400336+Vagacoder@users.noreply.github.com>
2018-03-20 23:50:28 -0700

    Add files via upload

---
+++ src/BigJavaCh13/TriangleTester.java
@@ -0,0 +1,10 @@
+package BigJavaCh13;
+
+public class TriangleTester {
+    public static void main(String[] args) {
+	Triangle t = new Triangle(10);
+	int area = t.getArea();
+	System.out.println("Area: " + area);
+	System.out.println("Expected: 55");
+    }
+}
