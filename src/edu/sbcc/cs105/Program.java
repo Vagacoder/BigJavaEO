5a462a98aef85d3ede59e13b966a94f522c7db8c
Vagacoder <qiruihu@gmail.com>
2018-03-30 16:38:47 -0700

    add files

--- src/edu/sbcc/cs105/Program.java
+++ src/edu/sbcc/cs105/Program.java
@@ -1,17 +1,9 @@
 package edu.sbcc.cs105;
 
-import java.util.*;
-
 public class Program {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Test06 t6 = new Test06();
-
-		ArrayList<Integer> list = t6.getList();
-
-		System.out.println(list.toString());
-
-	}
+    }
 
 }
--- src/edu/sbcc/cs105/Test06.java
+++ src/edu/sbcc/cs105/Test06.java
@@ -1,20 +1,10 @@
 package edu.sbcc.cs105;
 
-import java.util.*;
-
 public class Test06 {
 
-	public Test06() {
+    public static void main(String[] args) {
+	System.out.println("\u2655");
 
-	}
+    }
 
-	public ArrayList<Integer> getList() {
-
-		ArrayList<Integer> list = new ArrayList<>();
-
-		list.add(10);
-		list.add(99);
-
-		return list;
-	}
 }
